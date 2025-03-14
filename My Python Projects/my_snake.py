cb787f2524890498d5e870106385a27939cb2cf4
Martinez <dmartinez21@newyorklife.com>
2025-03-14 10:41:39 -0600

    day 20 theory

--- My Python Projects/my_snake.py
+++ My Python Projects/my_snake.py
@@ -10,22 +10,34 @@ class Snake:
             new_segment.penup()
             new_segment.goto(x_axis[_], y=0)
             self.segments.append(new_segment)
+
+        self.head = self.segments[0]
          
-    def up(self):
-        self.segments[0].setheading(90)
-    
-    def down(self):
-        self.segments[0].setheading(270)
-    
-    def right(self):
-        self.segments[0].setheading(0)
-
-    def left(self):
-        self.segments[0].setheading(180)
-
     def move(self):
         for segment in range(len(self.segments) - 1, 0, -1):
             coordinates = self.segments[segment - 1].position()
             self.segments[segment].goto(coordinates)
 
         self.segments[0].forward(20)
+
+    def up(self):
+        if self.head.heading() != 270:  
+            self.head.setheading(90)
+
+    def down(self):
+        if self.head.heading() == 90:
+            pass
+        else:
+            self.head.setheading(270)
+        
+    def right(self):
+        if self.head.heading() == 180:
+            pass 
+        else:
+            self.head.setheading(0)
+
+    def left(self):
+        if self.head.heading() == 0:
+            pass
+        else:
+            self.head.setheading(180) 
