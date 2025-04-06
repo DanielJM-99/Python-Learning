38d25a32ffc2d15a7ac1b6fc15a9124ea2ad6688
Martinez <dmartinez21@newyorklife.com>
2025-04-05 19:45:24 -0600

    latest pong

--- 100 Days of Python/DAY22/d_main_pong.py
+++ 100 Days of Python/DAY22/d_main_pong.py
@@ -1,28 +1,32 @@
 from turtle import Turtle, Screen
 import time 
 from d_paddles import Paddles
+from d_ball import Ball
 
 # 1: Create screen
 pong_screen = Screen()
-pong_screen.setup(width=1000, height=600)
+pong_screen.setup(width=800, height=600)
 pong_screen.bgcolor("black")
 pong_screen.tracer(0)
 
 # 2: Create paddle
-paddles = Paddles()
+u_paddle = Paddles((350, 0))
+c_paddle = Paddles((-350, 0))
+ball = Ball()
 
 # Listen to user input
 pong_screen.listen()
-pong_screen.onkeypress(key="Up", fun=paddles.move_up)
-pong_screen.onkeypress(key="Down", fun=paddles.move_down)
+pong_screen.onkeypress(key="Up", fun=u_paddle.move_up)
+pong_screen.onkeypress(key="Down", fun=u_paddle.move_down)
+
+pong_screen.onkeypress(key="w", fun=c_paddle.move_up)
+pong_screen.onkeypress(key="s", fun=c_paddle.move_down)
 
 game_on = True
 while game_on:
         # Screen update
         pong_screen.update()
-        time.sleep(0.2)
+        time.sleep(0.1)
+        ball.move_ball_r()
         
-        paddles.comp_paddle()
-
-
 pong_screen.exitonclick()
--- 100 Days of Python/DAY22/d_paddles.py
+++ 100 Days of Python/DAY22/d_paddles.py
@@ -1,60 +1,24 @@
 from turtle import Turtle
 
-U_POSITIONS = [(-480, 0), (-480, 20), (-480, 40), (-480, 60)]
-C_POSITIONS = [(480, 0), (480, 20), (480, 40), (480, 60)]
 UP = 90
 DOWN = 270
 
 class Paddles(Turtle):
 
-    def __init__(self):
+    def __init__(self, start_pos):
         super().__init__()
-
-        self.padl_seg_user = []
-        self.create_paddle(U_POSITIONS, self.padl_seg_user)
-
-        self.padl_seg_comp = []
-        self.create_paddle(C_POSITIONS, self.padl_seg_comp)
-        
-    def create_paddle(self, positions, pd_segments):
-        for starting_position in positions:
-            pd_seg = Turtle("square")
-            pd_seg.color("white")
-            pd_seg.penup()
-            pd_seg.goto(starting_position)
-            pd_segments.append(pd_seg)
-
-    def move_paddle(self, segm_list, direction):
-        for segment in range(len(segm_list)):
-            segm_list[segment].setheading(direction)
-            segm_list[segment].forward(20)
+        self.shape("square")
+        self.color("white")
+        self.penup()
+        self.shapesize(stretch_len=1, stretch_wid=5)
+        self.goto(start_pos)
         
     def move_up(self):
-        # Head upwards
-        if self.padl_seg_user[-1].ycor() < 280:
-            self.move_paddle(self.padl_seg_user, UP)
+        if self.ycor() < 260:
+            new_y = self.ycor() + 20
+            self.goto(self.xcor(), new_y)
 
     def move_down(self):
-        # Head downwards
-        if self.padl_seg_user[0].ycor() > -280:
-            self.move_paddle(self.padl_seg_user, DOWN)
-
-    def move_paddle_comp(self):
-        for seg in range(len(self.padl_seg_comp) - 1, 0, -1):
-            coordinates = self.padl_seg_comp[seg - 1].position()
-            self.padl_seg_comp[seg].goto(coordinates)
-        
-    def comp_paddle(self):
-        if self.padl_seg_comp[0].ycor() > -280:
-            self.padl_seg_comp[0].setheading(DOWN)
-            self.move_paddle_comp()
-            self.padl_seg_comp[0].forward(20)
-        else:
-            self.padl_seg_comp.reverse()
-            if self.padl_seg_comp[0].ycor() < 280:
-                self.padl_seg_comp[0].setheading(UP)
-                self.move_paddle_comp()
-                self.padl_seg_comp[0].forward(20)
-
-
-
+        if self.ycor() > -260:
+            new_y = self.ycor() - 20
+            self.goto(self.xcor(), new_y)
