f845bf4629c16b42ddc6f4e0d639cbc8fc40b982
Martinez <dmartinez21@newyorklife.com>
2025-04-22 14:52:31 -0600

    day24

--- 100 Days of Python/DAY24/Mail Merge Project Start/d_main.py
+++ 100 Days of Python/DAY24/Mail Merge Project Start/d_main.py
@@ -1,11 +1,8 @@
 with open("./100 Days of Python/DAY24/Mail Merge Project Start/Input/Letters/starting_letter.txt") as letter:
     content = letter.read()
-    print(content)
 
 with open("./100 Days of Python/DAY24/Mail Merge Project Start/Input/Names/invited_names.txt") as names:
     all_names = names.readlines()
-    print(all_names)
-
 
 for name in all_names:
     new_name = name.strip()
