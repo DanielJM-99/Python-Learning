0dabd2f1298a05c30494263dce3f05683e62f283
Martinez <dmartinez21@newyorklife.com>
2025-04-23 14:51:46 -0600

    day24 beginning

---
+++ 100 Days of Python/DAY25/squirrel_colors.py
@@ -0,0 +1,31 @@
+import pandas 
+
+# Create a CSV with the sum of the fur colors (Primary Fur Color) in a small table 
+
+# Read data
+squirrel_data = pandas.read_csv("./100 Days of Python/DAY25/2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+
+# Only select required column
+fur_colors = squirrel_data["Primary Fur Color"]
+
+# Converto into list
+colors_list = fur_colors.to_list()
+
+# Get unique values
+unique_colors = list(set(colors_list))
+print(unique_colors)
+
+# Count unique values and create ditionary
+color_dictionary = {}
+for color in unique_colors:
+    if pandas.isna(color):
+        pass
+    else:
+        color_count = colors_list.count(color)
+        color_dictionary[color] = color_count
+print(color_dictionary)
+
+# Create dataframe then csv
+count_data = pandas.DataFrame([color_dictionary])
+print(count_data)
+count_data.to_csv("squirrel_colors.csv")
