dbf1346c6984a16b5061379685b445d01404feb0
DanielJM-99 <danyjm99@hotmail.com>
2025-04-24 20:03:12 -0600

    changes

--- 100 Days of Python/DAY24/read_files_with.py
+++ 100 Days of Python/DAY24/read_files_with.py
@@ -16,6 +16,18 @@
 # with open("./100 Days of Python/DAY24/new_file.txt", mode="w") as file:
 #     file.write("First comment on new file on current dir, current folder day 24")
 
-with open("../../Desktop/new_file.txt") as file:
-    text = file.read()
-    print(text)
+# with open("../../Desktop/new_file.txt") as file:
+#     text = file.read()
+#     print(text)
+    
+# import OS module
+import os
+
+# Get the list of all files and directories
+path = r"C:\Users\DanielJM\Downloads"
+dir_list = os.listdir(path)
+print("Files and directories in '", path, "' :")
+
+# prints all files
+for items in dir_list:
+    print(items)
